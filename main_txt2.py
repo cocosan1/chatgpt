20a0bf1d9f785814b09183d966d9478509bf63a0
hskw1 <isishskw@gmail.com>
2023-07-03 20:52:17 +0900

    6th commit

--- main_txt2.py
+++ main_txt2.py
@@ -132,113 +132,28 @@ def make_index():
 
     st.info('index化完了')
 
-    # response変数を初期化する
-    response = None
+def qa_calc2():
 
-def make_index2():
-##########################google driveからテキストファイルの取得
-    #current working dir
-    cwd = os.path.dirname(__file__)
+    #質問の入力
+    question = st.text_input('質問を入力してください')
 
-    #**********************gdriveからテキストファイルのダウンロード・df化
+    if not question:
+        st.info('質問を入力してください')
+        st.stop()
 
-    # Google Drive APIを使用するための認証情報を取得する
-    creds_dict = st.secrets["gcp_service_account"]
-    creds = service_account.Credentials.from_service_account_info(creds_dict)
-
-    # Drive APIのクライアントを作成する
-    #API名（ここでは"drive"）、APIのバージョン（ここでは"v3"）、および認証情報を指定
-    service = build("drive", "v3", credentials=creds)
-
-    #フォルダtexts内にあるファイルリストを作成
-    folder_id = st.secrets["ID"]  # 取得したいフォルダのIDを指定してください
-
-    # フォルダ内のファイルを取得するためのクエリ
-    query = f"'{folder_id}' in parents"
-
-    # フォルダ内のファイル名を取得するリクエスト
-    response = service.files().list(q=query, fields="files(name)").execute()
-
-    # レスポンスからファイル名を取得する
-    files = response.get("files", [])
-
-
-    for fname in files:
-        # 指定したファイル名を持つファイルのIDを取得する
-        #Google Drive上のファイルを検索するためのクエリを指定して、ファイルの検索を実行します。
-        # この場合、ファイル名とMIMEタイプを指定しています。
-
-        file_name = fname["name"]
-        query = f"name='{file_name}' and mimeType='text/plain'"
-
-
-        #指定されたファイルのメディアを取得
-        results = service.files().list(q=query).execute()
-        items = results.get("files", [])
-
-        if not items:
-            st.caption(f"No files found with name: {file_name}")
-        else:
-            # ファイルをダウンロードする
-            file_id = items[0]["id"]
-            file = service.files().get(fileId=file_id).execute()
-            file_content = service.files().get_media(fileId=file_id).execute()
-
-            # ファイルを保存する
-            file_path = os.path.join(cwd, 'texts', file_name)
-            with open(file_path, "wb") as f:
-                f.write(file_content)
-        
-    ###########################テキストデータの統合
-    #検索するディレクトリを指定する
-    directory = os.getcwd() #このPythonファイルが保存されているフォルダを指定
-
-    #txt_files というリストに拡張子が.txtのファイルパスを入れる
-    txt_files = glob.glob(directory + "/texts/*.txt")
-
-    #リストが空の場合は終了する
-    if not txt_files:
-        input('拡張子が.txtのファイルが見つかりませんでした Enterで終了します')
-        sys.exit() #プログラムを終了させる
-
-    #結合後のファイル名を指定する
-    merged_file_name = "./main/main.txt"
-
-    # ファイルを開いて結合する------------------------------------------
-    #結合後のファイルを開く(W:ファイルを書き込み用に開き中身を削除する。ファイルが存在しなければ作成)
-    with open(merged_file_name, "w",encoding="utf-8") as merged_file:
-        #リスト内の項目を1つずつ処理する
-        for file_name in txt_files:
-
-            #結合後のファイルは対象外にする
-            if merged_file_name in file_name:
-                continue
-
-            #リスト内のファイルを読み取り専用で開く
-            with open(file_name, "r",encoding="utf-8") as file:
-                #read_textという変数にファイル内のすべてのデータを代入
-                read_text = file.read()
-                #もし最後が改行で終わっていない場合は改行を追加する
-                if not read_text == '' and not read_text.endswith('\n'):
-                        read_text = read_text + '\n'
-                #結合後のファイルに書き込む
-                merged_file.write(read_text)
-
-    ###########################テキストデータの読み込み・index化
-    documents = SimpleDirectoryReader(
-        input_dir="./main").load_data()
-
-    #GPTListIndexを使ってノードに分割した後、OpenAIのモデルを使ってベクトルにしました。
-    # 特にid_to_text_mapはFaissReaderで期待される形にしておきました。
-    # GPTVectorStoreIndexの作成
-    list_index = GPTListIndex.from_documents(documents)
+    #ストレージからindexデータの読み込み
+    storage_context = StorageContext.from_defaults(
+        docstore=SimpleDocumentStore.from_persist_dir(persist_dir="./storage_context"),
+        vector_store=SimpleVectorStore.from_persist_dir(persist_dir="./storage_context"),
+        index_store=SimpleIndexStore.from_persist_dir(persist_dir="./storage_context"),
+    )
 
     # 実装時点でデフォルトはtext-ada-embedding-002
     embed_model = OpenAIEmbedding()
 
     docs = []
     id_to_text_map = {}
-    for i, (_, node) in enumerate(list_index.storage_context.docstore.docs.items()):
+    for i, (_, node) in enumerate(storage_context.docstore.docs.items()):
         text = node.get_text()
         docs.append(embed_model.get_text_embedding(text))
         id_to_text_map[i] = text
@@ -252,18 +167,18 @@ def make_index2():
 
     #クエリをベクトル化し、kでFaissで何個の似たベクトルを探すか指定して、FaissReaderで類似するテキストを取得しました。
     # クエリとFaissから取り出すノード数の設定
-    query_text = "ハイエースの荷台のスペースを教えて"
+    query_text = question
     k = 2
 
-    # ベクトル化
+    # questionのベクトル化
     query = embed_model.get_text_embedding(query_text)
     query=np.array([query])
 
-    # Faissからのノードの取り出し
+    # Faissからのquestionに近いノードの取り出し2個
     reader = FaissReader(index)
     documents = reader.load_data(query=query, id_to_text_map=id_to_text_map, k=k)
 
-    st.write(len(documents))
+    st.write(f'count_node: {len(documents)}')
 
     #Faissで確認した類似したノードを使って、GPTListIndexを作成しました。
     # デバッグ用
@@ -274,22 +189,29 @@ def make_index2():
     # GPTListIndexの作成
     index = GPTListIndex.from_documents(documents, service_context=service_context)
 
-    #作成したインデックスを使って回答の生成を実行しました。
-    query_engine = index.as_query_engine()
-    response = query_engine.query(query_text)
+    QA_PROMPT_TMPL = (
+    "私たちは以下の情報をコンテキスト情報として与えます。 \n"
+    "---------------------\n"
+    "{context_str}"
+    "\n---------------------\n"
+    "あなたはAIとして、この情報をもとに質問を日本語で答えます。前回と同じ回答の場合は同じ回答を行います。: {query_str}\n"
+    )
+    QA_PROMPT = QuestionAnswerPrompt(QA_PROMPT_TMPL)
+
+    query_engine = index.as_query_engine(text_qa_template=QA_PROMPT)
+
+    # テンプレを送る
+    response = query_engine.query(question)
 
     for i in response.response.split("。"):
         st.write(i + "。")
 
 
+    # #ソースの表示
+    # st.write(response.source_nodes)
+  
 
-
-
-
-
-   
-
-def q_and_a():
+def qa_calc():
 
     #質問の入力
     question = st.text_input('質問を入力してください')
@@ -322,11 +244,6 @@ def q_and_a():
     # 質問を行う
     response = query_engine.query(question)
 
-
-    # #基本形 ベクター検索 + Chat Completion API 実行
-    # query_engine = index.as_query_engine()
-    # response = query_engine.query(question)
-
     for i in response.response.split("。"):
         st.write(i + "。")
 
@@ -337,11 +254,9 @@ def q_and_a():
 def main():
     # アプリケーション名と対応する関数のマッピング
     apps = {
-        'Q&A': q_and_a,
+        'Q&A2': qa_calc2,
+        'Q&A': qa_calc,
         'txtのindex化': make_index,
-        'txtのindex化2': make_index2,
-
-
     }
     selected_app_name = st.selectbox(label='項目の選択',
                                              options=list(apps.keys()))
